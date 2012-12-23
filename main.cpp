55329df35107ac557e5a3c64c7b2d8f953b7d85e
dualprostudios <altan.jazz@hotmail.com>
2012-12-23 07:02:59 +0100

    Adding some code.
    Didn't have all the code so just typed from my memory.

---
+++ main.cpp
@@ -0,0 +1,7 @@
+/* 
+main.cpp
+This project uses SDL and OpenGL in order to function, so if you are going to compile the sources, make sure you
+include the .dll's required! 
+*/
+#include "SDL/SDL.h"
+#include <ofstream>
